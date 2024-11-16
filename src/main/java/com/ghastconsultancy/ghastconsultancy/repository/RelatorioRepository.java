2f5d0cd7f654114b79bada258ddfece2de03ff15
Felipe Duan <felipeduan1@gmail.com>
2024-11-16 00:43:58 -0300

    adc RelattÃorioRepository

---
+++ src/main/java/com/ghastconsultancy/ghastconsultancy/repository/RelatorioRepository.java
@@ -0,0 +1,8 @@
+package com.ghastconsultancy.ghastconsultancy.repository;
+
+import com.ghastconsultancy.ghastconsultancy.model.Relatorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
+
+}
