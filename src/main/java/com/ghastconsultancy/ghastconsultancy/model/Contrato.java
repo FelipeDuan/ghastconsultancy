84370dcaa106099a81b2042077e3cc80e183aa8d
Felipe Duan <felipeduan1@gmail.com>
2024-11-19 12:59:14 -0300

    cors habilitado

--- src/main/java/com/ghastconsultancy/ghastconsultancy/model/Contrato.java
+++ src/main/java/com/ghastconsultancy/ghastconsultancy/model/Contrato.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDate;
 
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @Table(name = "contratos")
 @NoArgsConstructor
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class Contrato {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
